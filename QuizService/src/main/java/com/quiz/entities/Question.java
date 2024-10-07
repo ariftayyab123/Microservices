d6d92b320b1835ba2784c3e89e154d41fea9d5e2
TayyabArif <98744758+ariftayyab123@users.noreply.github.com>
2024-10-07 14:25:12 +0530

    Add files via upload

---
+++ QuizService/src/main/java/com/quiz/entities/Question.java
@@ -0,0 +1,23 @@
+package com.quiz.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Question {
+
+    private Long questionId;
+    private String question;
+    private Long quizId;
+    private int uploadedVia;
+}
